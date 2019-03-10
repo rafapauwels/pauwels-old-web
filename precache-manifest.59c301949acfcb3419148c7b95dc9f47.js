d220fd00422d38e2a9e6dad2f4891883b9dd47b1
Rafael Pauwels <rafapauwels@gmail.com>
2019-03-10 19:11:36 -0300

    Updates

---
+++ precache-manifest.59c301949acfcb3419148c7b95dc9f47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "95ef884d1de3077ab006",
+    "url": "/static/js/main.668697d7.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "95ef884d1de3077ab006",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "2fdd05a20706c05219c4b8aafe37e62e",
+    "url": "/index.html"
+  }
+];
