c1b976b28798c47dfe224b5277a79e875a2de340
Rafael Pauwels <rafapauwels@gmail.com>
2019-03-10 19:28:38 -0300

    Changing favicon and page title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Currently under construction.</h1>
+        <h1>Currently under construction</h1>
       </div>
     );
   }
