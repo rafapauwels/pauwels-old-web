c17974376ce7c900ad131d99c14ff5ecdaf97c1f
Rafael Pauwels <rafapauwels@gmail.com>
2019-03-12 00:24:02 -0300

    Made Home responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,10 +6,10 @@ let Home = () => (
     <p className="action-call">Hi.</p>
     <p>I'm a software developer passionate about crafting beautiful, responsive and accessible websites, supported by blazzing fast and scalable backends.</p>
     <div className="button-group">
-      <a className="work-button" href="https://github.com/rafapauwels" target="_blank">work</a>
-      <a className="blog-button">blog</a>
+      <a className="work-button" href="https://github.com/rafapauwels" target="_blank" rel="noopener noreferrer">work</a>
+      <a className="blog-button" href="/blog">blog</a>
     </div>
   </section>
-)
+) 
 
 export default Home
