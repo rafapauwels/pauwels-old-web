1b390beff0b13a6e6b4d3c23a403d5f7a8d70af0
Rafael Pauwels <rafapauwels@gmail.com>
2019-03-13 00:55:23 -0300

    Made contact page responsive
    WIP: contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,8 +8,10 @@ let Contact = () => (
       <h2>But don't be shy about the timezone,<br/>I'm always up.</h2>
     </section>
     <section className="talk-to-me">
-      <h1>Let's talk</h1>
-      <h2>Share your thoughts with me</h2>
+      <div className="whole-form">
+        <h1>Let's talk</h1>
+        <h2>Share your thoughts with me</h2>
+      </div>
     </section>
   </div>
 )
