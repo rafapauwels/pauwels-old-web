2f82641f8f26402b58daa4320b32a7de7bc59c62
Rafael Pauwels <rafapauwels@gmail.com>
2019-03-17 16:02:44 -0300

    Remade Contact page
    Now there is no dark box in the left, everything is super clear
    The mockup for the API connection is already in place

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,19 +4,11 @@ import './index.scss'
 import ContactForm from '../ContactForm'
 
 let Contact = () => (
-  <div className="contact-page">
-    <section className="im-here">
-      <h1>I'm currently in Brazil</h1>
-      <h2>But don't be shy about the timezone,<br/>I'm always up.</h2>
-    </section>
-    <section className="talk-to-me">
-      <div className="whole-form">
-        <h1>Let's talk</h1>
-        <h2>Share your thoughts with me</h2>
-        <ContactForm />
-      </div>
-    </section>
-  </div>
+  <section className="contact-page">
+    <h1>Let's talk.</h1>
+    <h2>How can I help?</h2>
+    <ContactForm />
+  </section>
 )
 
 export default Contact
--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -3,50 +3,57 @@ import './index.scss'
 
 export default class ContactForm extends Component {
   
-  state = {name: '', email: '', message: '', selected: '', step: 0}
+  state = {name: '', email: '', subject: '', message: ''}
   
   render() {      
-    const proxPag = () => { this.setState({ step: 1 }) }
-    const handleNameChange = nameEvent => { this.setState({ name: nameEvent.target.value }) }
-    const handleOptionChange = changeEvent => { this.setState({ selected: changeEvent.target.value }) }
-    const handlePageChange = formSubmitEvent => { formSubmitEvent.preventDefault(); proxPag(); }
+    const handleNameChange = e => { this.setState({ name: e.target.value }) }
+    const handleEmailChange = e => { this.setState({ email: e.target.value }) }
+    const handleSubjectChange = e => { this.setState({ subject: e.target.value }) }
+    const handleMessageChange = e => { this.setState({ message: e.target.value }) }
 
-    const firstPag = () => (
-      <form onSubmit={handlePageChange}>
-        <label class="how-call">
-          So to begin, how should I call you?
-          <input className="name-input" type="text" name="name" value={this.state.name} onChange={handleNameChange} />
-        </label>
+    const sendEmail = e => {
+      e.preventDefault()
 
-        <label class="about-what">and about what do you want to talk?</label>
-        <label>
-          <input type="radio" value="hiring" onChange={handleOptionChange} checked={this.state.selected === "hiring"} />
-          Hiring/Freelance work
-        </label>
-        <label>
-          <input type="radio" value="meeting" onChange={handleOptionChange} checked={this.state.selected === "meeting"} />
-          Meeting for a coffee
-        </label>
-        <label>
-          <input type="radio" value="other" onChange={handleOptionChange} checked={this.state.selected === "other"} />
-          Other
-        </label>
-
-        <button type="submit">next</button>
-      </form>
-    )
-
-    const segPag = () => (
-      console.log(this.state)
-    )
+      // Change button send to sending and make unclicable
+    
+      // Make post, if success change 'send' to 'sent' and unclicable unless any stated is altered
+      // if fail show red fail and shows the user the user a 'mailto:' button
+      fetch('myHerokuApi/api/contact/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+          subject: this.state.subject,
+          message: this.state.message,
+        })
+      }).then(response => console.log(response))
+    }
 
     return (
         <div className="contact-form">
-          {this.state.step === 0 ?
-            firstPag() :
-            segPag()
-          }
-          <p>*your email won't be stored or shared in anyway.</p>
+          <form onSubmit={sendEmail}>
+            <label className="name-label"> Name
+              <input className="name-input" type="text" name="name" value={this.state.name} onChange={handleNameChange} required/>
+            </label>
+
+            <label className="email-label"> Email
+              <input className="email-input" type="email" name="email" value={this.state.email} onChange={handleEmailChange} required/>
+            </label>
+
+            <label className="subject-label"> Subject
+              <input className="subject-input" type="text" name="subject" value={this.state.subject} onChange={handleSubjectChange} required/>
+            </label>
+
+            <label className="message-label"> Message
+              <textarea className="message-input" type="text" name="message" value={this.state.message} onChange={handleMessageChange} required/>
+            </label>
+            <button type="submit">send</button>
+          </form>
+          <footer>*your email won't be stored or shared in anyway.</footer>
         </div>
       )
   }
